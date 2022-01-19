3b1ccdbe6aff3f90401168b6adc7781a59d40079
Anastasia Panova <apanova@sportmaster.ru>
2022-01-20 00:39:34 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.scss';
+import '../assets/images/background.jpg';
+import '../assets/images/background2.jpg';
+import '../assets/images/background3.jpg';
+
+console.log('запускается 2 заза?')
+
